df16055344c432838f1f7288c64bbe557345b754
Ahad Ramzan <140896742+Ahad-Ramzan@users.noreply.github.com>
2025-02-06 16:12:22 +0500

    add git_practice

---
+++ frontend/components/git_practice/page.tsx
@@ -0,0 +1,11 @@
+const page = () => {
+  return (
+    <div>
+      <div className="container mx-auto h-screen flex flex-col items-center justify-center">
+        <h1 className="text-3xl font-bold">Git Practice</h1>
+      </div>
+    </div>
+  );
+};
+
+export default page;
