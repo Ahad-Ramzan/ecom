580c7b67cfff2c0795bcd98bd97e56ae4a1c5eae
Ahad Ramzan <140896742+Ahad-Ramzan@users.noreply.github.com>
2025-02-06 16:15:15 +0500

    edit page

--- frontend/components/git_practice/page.tsx
+++ frontend/components/git_practice/page.tsx
@@ -3,6 +3,10 @@ const page = () => {
     <div>
       <div className="container mx-auto h-screen flex flex-col items-center justify-center">
         <h1 className="text-3xl font-bold">Git Practice</h1>
+        <p className="text-xl">This is a practice page for Git and GitHub.</p>
+        <p className="text-xl">
+          You can use this page to practice Git and GitHub.
+        </p>
       </div>
     </div>
   );
